768b9bb4131e462439c5ba08a0f36deceb5208d9
asdadads <jas31goh@gmail.com>
2022-03-22 02:56:15 +0800

    data is sorted but in a resource-wasting way

--- handlebars.js
+++ handlebars.js
@@ -1,52 +1,40 @@
 var data = [
     {
-        category: 'BUSINESS',
-        articles:
-        [
-            {
-                headline: "Oil prices break US$100, S'pore stocks sink as Russia launches military attack on Ukraine",
-                feature: false,
-                art_url: 'images/maria-lupan-hy97yy3e03A-unsplash.jpg',
-                date: '24 FEB 2022'
-            },
-            {
-                headline: "Nations vet 3,000-page handbook to halt climate crisis",
-                feature: false,
-                art_url: 'images/patrick-hendry-6xeDIZgoPaw-unsplash.jpg',
-                date: '21 MAR 2022'
-            }
-        ]
+        headline: "Nations vet 3,000-page handbook to halt climate crisis",
+        category: 'ASIA',
+        featured: false,
+        art_url: 'images/patrick-hendry-6xeDIZgoPaw-unsplash.jpg',
+        date: '21 MAR 2022',
+        digital_date: new Date('03/21/2022 12:00:00 AM')
     },
     {
+        headline: "Oil prices break US$100, S'pore stocks sink as Russia launches military attack on Ukraine",
+        category: 'BUSINESS',
+        featured: false,
+        art_url: 'images/maria-lupan-hy97yy3e03A-unsplash.jpg',
+        date: '24 FEB 2022',
+        digital_date: new Date('02/24/2022 12:00:00 AM')
+    },
+    {
+        headline: "Hong Kong to lift flight ban on 9 countries in April, cut quarantine to 7 days for travellers",
         category: 'ASIA',
-        articles:
-        [
-            {
-                headline: "Hong Kong to lift flight ban on 9 countries in April, cut quarantine to 7 days for travellers",
-                feature: true,
-                feature_text: 'Up to 4 people will be allowed for public gatherings, up from the current 2.',
-                art_url: 'images/marco-lopez-qK6HAkB91Yc-unsplash.jpg',
-                date: '21 MAR 2022'
-            }
-            
-        ]
+        featured: true,
+        featured_text: 'Up to 4 people will be allowed for public gatherings, up from the current 2.',
+        art_url: 'images/marco-lopez-qK6HAkB91Yc-unsplash.jpg',
+        date: '21 MAR 2022',
+        digital_date: new Date('03/21/2022 12:00:00 AM')
     }
 ]
 
 Handlebars.registerHelper('find_featured', function(options) {
-    for (i in data)
-    {
-        var category_articles = data[i].articles;
-        for (j in category_articles)
-        {
-            var article =  category_articles[j];
-            if (article.feature) return options.fn(article);
-        }
+    for (i in data) {
+        var article = data[i]
+        if (article.featured) return options.fn(article);
     }
 });
 
 Handlebars.registerHelper('sort', function(options) {
-
+    data.sort((a,b) => a.digital_date - b.digital_date);
 })
 
 function renderHTML(templateId, selector) {
