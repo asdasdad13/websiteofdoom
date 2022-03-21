93b31dfa1957e3b0c9d20971c3442dec802e4c66
asdadads <jas31goh@gmail.com>
2022-03-21 15:30:33 +0800

    removed row added flex for news ticker

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 window.onload = function() {
+    //call functions here
+    loadBreakingNews();
+    loadDate();
+}
+
+function loadBreakingNews() {
     var breakingNewsList = document.getElementById('breaking-news-list');
     var headlines = ['Headline 1','Headline 2','Headline 3'];
     for (i in headlines)
@@ -9,6 +15,9 @@ window.onload = function() {
         new_li.className = 'd-inline';
         breakingNewsList.appendChild(new_li);
     }
+}
+
+function loadDate() {
     var date = new Date();
     var y = date.getFullYear();
     switch (date.getMonth())
@@ -77,74 +86,3 @@ window.onload = function() {
     }
     document.getElementById('current_date').innerHTML = day + ', ' + m + ' ' + d + ', ' + y;
 }
-
-// function displayDate() {
-//     console.log(1)
-//     var date = new Date();
-//     var y = date.getFullYear();
-//     switch (date.getMonth())
-//     {
-//         case 0:
-//             m = "JANUARY";
-//             break;
-//         case 1:
-//             m = "FEBRUARY";
-//             break;
-//         case 2:
-//             m = "MARCH";
-//             break;
-//         case 3:
-//             m = "APRIL";
-//             break;
-//         case 4:
-//             m = "MAY";
-//             break;
-//         case 5:
-//             m = "JUNE";
-//             break;
-//         case 6:
-//             m = "JULY";
-//             break;
-//         case 7:
-//             m = "AUGUST";
-//             break;
-//         case 8:
-//             m = "SEPTEMBER";
-//             break;    
-//         case 9:
-//             m = "OCTOBER";
-//             break;
-//         case 10:
-//             m = "NOVEMBER";
-//             break;
-//         case 11:
-//             m = "DECEMBER";
-//             break;
-//     }
-//     var d = date.getDate();
-//     switch (date.getDay())
-//     {
-//         case 0:
-//             day = "SUNDAY";
-//             break;
-//         case 1:
-//             day = "MONDAY";
-//             break;
-//         case 2:
-//             day = "TUESDAY";
-//             break;
-//         case 3:
-//             day = "WEDNESDAY";
-//             break;
-//         case 4:
-//             day = "THURSDAY";
-//             break;
-//         case 5:
-//             day = "FRIDAY";
-//             break;
-//         case 6:
-//             day = "SATURDAY";
-//             break;
-//     }
-//     document.getElementById('current_date').innerHTML = day + ', ' + m + ' ' + d + ', ' + y;
-// }
