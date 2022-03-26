2913c0bceb22ca44ddf59ae4c7af10d17d9abb77
asdadads <jas31goh@gmail.com>
2022-03-26 23:08:39 +0800

    fixed padding bottom of cards

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+var btt = document.getElementById('btn-back-to-top')
+
+window.onscroll = function() {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      btt.style.display = "block";
+    } else {
+      btt.style.display = "none";
+    }
+  }
+
+btt.addEventListener('click', backToTop);
+
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 function loadBreakingNews() {
     var breakingNewsList = document.getElementById('breaking-news-list');
     var headlines = ['Headline 1','Headline 2','Headline 3'];
