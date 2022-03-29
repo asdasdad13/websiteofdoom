2616283d43e2bb98b0d4521f0ae4f895768b8b9a
asdasdad13 <56213547+asdasdad13@users.noreply.github.com>
2022-03-29 16:11:22 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var btt = document.getElementById('btn-back-to-top')
-
+//for the back to top button
 window.onscroll = function() {
     scrollFunction();
 }
 
-function scrollFunction() {
+function scrollFunction() { //for the back to top button
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
       btt.style.display = "block";
     } else {
@@ -13,12 +13,12 @@ function scrollFunction() {
   }
 
 
-function backToTop() {
+function backToTop() { //for the back to top button
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
 
-function loadBreakingNews() {
+function loadBreakingNews() { //loads lst of breaking news for the ticker
     var breakingNewsList = document.querySelector('#breaking-news ul');
     var headlines = ['Russians against war are fleeing state crackdowns', 'Has Turkey dodged its sunflower oil crisis?','Can Ukraine\'s internet sustain longer-term attacks and destruction from Russia?']; //random data
     for (i in headlines)
@@ -33,12 +33,12 @@ function loadBreakingNews() {
     }
 }
 
-function loadDate() {
+function loadDate() { //loads date that appears under navbar on homepage
     [day,m,d,y] = getDate(new Date());
     document.getElementById('current_date').innerHTML = day + ', ' + m + ' ' + d + ', ' + y;
 }
 
-function getDate(date) {
+function getDate(date) { //gets a date in human readable format
     var y = date.getFullYear();
     switch (date.getMonth())
     {
@@ -107,7 +107,7 @@ function getDate(date) {
     return [day,m,d,y];
 }
 
-function updatedAgo(publishedTime) {
+function updatedAgo(publishedTime) { //calculates the time since the articles were published
     var seconds = Math.floor((new Date() - publishedTime) / 1000);
     var interval = Math.floor(seconds / 31536000);
 
@@ -133,7 +133,7 @@ function updatedAgo(publishedTime) {
     return Math.floor(seconds) + " seconds";
 }
 
-function fillUpdatedAgos() {
+function fillUpdatedAgos() { //load times into latest-news-col
     document.getElementsByClassName('updatedAt')[0].innerHTML = updatedAgo(new Date('2022-03-27T16:15:00Z'));
     document.getElementsByClassName('updatedAt')[1].innerHTML = updatedAgo(new Date('2022-03-26T20:04:00Z'));
     document.getElementsByClassName('updatedAt')[2].innerHTML = updatedAgo(new Date('2022-03-25T18:19:00Z'));
@@ -142,17 +142,17 @@ function fillUpdatedAgos() {
     document.getElementsByClassName('updatedAt')[5].innerHTML = updatedAgo(new Date('2022-03-17T10:31:00Z'));
 }
 
-var mapClrs = {
+var mapClrs = { //text and bg colour change in dark mode toggle
     dark: 'light',
     light: 'dark',
 }
 
-var mapIcons = {
+var mapIcons = { //change icon of dark mode button for each toggle
     'fa-sun': 'fa-moon',
     'fa-moon': 'fa-sun'
 }
 
-function darkMode() {
+function darkMode() { //switches between dark and light mode by exchanging text-light -> text-dark, bg-light, bg-dark and vice versa
     var element = document.getElementById('page');
     element.outerHTML = element.outerHTML.replace(/dark|light/gi, function(matched) {
         //simultaneously changes function name for btn-dark-mode
@@ -165,12 +165,13 @@ function darkMode() {
     });
 }
 
-function insertDarkModeButton() {
+function insertDarkModeButton() { //for initialising the dark mode button
     var i = document.createElement('i');
     i.setAttribute('class','fa-solid fa-moon p-2')
     document.getElementById('btn-dark-mode').appendChild(i);
 }
 
+//call all functions below this line
 loadBreakingNews();
 loadDate();
 insertDarkModeButton();
